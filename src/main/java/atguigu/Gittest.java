248bcfcd33230f47b7edb8123930ee43f4fa95a5
lihu <1611571263@qq.com>
2021-10-29 10:37:51 +0800

    First commit idea

---
+++ src/main/java/atguigu/Gittest.java
@@ -0,0 +1,9 @@
+package atguigu;
+
+public class Gittest {
+    public static void main(String[] args) {
+        System.out.println("hello atguigu");
+        System.out.println("hello atguigu");
+        System.out.println("hello atguigu");
+    }
+}
